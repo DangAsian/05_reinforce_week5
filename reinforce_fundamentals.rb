5b1e69ac01af9aa8b2be72d187995e00f4598a79
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-28 16:25:03 -0400

    [05_reinforce] completed seat arrangement

---
+++ reinforce_fundamentals.rb
@@ -0,0 +1,21 @@
+a = [
+  [nil, "Pumpkin", nil, nil],
+  ["Socks", nil, "Mimi", nil],
+  ["Gismo", "Shadow", nil, nil],
+  ["Smokey","Toast","Pacha","Mau"]
+]
+
+a.each.with_index do |row_value, row_index|
+  row_value.each.with_index do |seat_value, seat_index|
+    if seat_value == nil
+      puts "Row #{row_index + 1} seat #{seat_index + 1} is free. Do you want to sit there? (y/n)"
+      user_input = gets.chomp
+        if user_input == "y"
+          puts "What is your name?"
+          name = gets.chomp
+          row_value[seat_index] = name
+          end
+      end
+    end
+  end
+  puts a
